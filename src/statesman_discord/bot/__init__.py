7c3c454fa19e2dbd9826ed675c6cbe57fa6d3e65
Paul Schifferer <paul@schifferers.net>
2023-01-03 17:16:20 -0800

    Bot

--- src/statesman_discord/bot/__init__.py
+++ src/statesman_discord/bot/__init__.py
@@ -3,3 +3,46 @@ __author__ = "Paul Schifferer <paul@schifferers.net>"
 bot/
 -
 """
+
+import discord
+from discord.ext import commands
+import logging
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.typing = False
+intents.presences = False
+intents.members = False
+
+
+bot = commands.Bot(command_prefix=commands.when_mentioned, description="TBD", intents=intents)
+# logger = logging.getLogger("root")
+
+
+@bot.event
+async def on_ready():
+    logging.info("Logged on as %s (%s).", bot.user, bot.user.id)
+
+
+#     async def on_message(self, message):
+#         if message.author == self.user:
+#             logging.debug("Ignoring message from ourselves.")
+#             return
+
+#         print(f"Message from {message.author}: {message.content}")
+
+
+@bot.command()
+async def add(ctx, param: int):
+    """xxx
+
+    Args:
+        ctx (_type_): _description_
+        param (int): _description_
+    """
+    logging.info("add: %d", param)
+    await ctx.send(param)
+
+
+# bot = DiscordBot(command_prefix="#", intents=intents)
--- src/statesman_discord/main.py
+++ src/statesman_discord/main.py
@@ -8,7 +8,6 @@ import os, sys
 import discord
 import logging
 from logging.config import dictConfig
-from statesman_discord.bot.client import DiscordClient
 from dotenv import load_dotenv, find_dotenv
 
 
@@ -16,26 +15,26 @@ ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
 
-dictConfig(
-    {
-        "version": 1,
-        "formatters": {
-            "default": {
-                "format": "[%(asctime)s] %(levelname)s %(module)s, line %(lineno)d: %(message)s",
-            }
-        },
-        "handlers": {"console": {"class": "logging.StreamHandler", "stream": "ext://sys.stdout", "formatter": "default"}},
-        "root": {"level": "INFO", "handlers": ["console"]},
-    }
-)
+# dictConfig(
+#     {
+#         "version": 1,
+#         "formatters": {
+#             "default": {
+#                 "format": "[%(asctime)s] %(levelname)s %(module)s, line %(lineno)d: %(message)s",
+#             }
+#         },
+#         "handlers": {"console": {"class": "logging.StreamHandler", "stream": "ext://sys.stdout", "formatter": "default"}},
+#         "root": {"level": "INFO", "handlers": ["console"]},
+#     }
+# )
 
-intents = discord.Intents.default()
-intents.message_content = True
+discord.utils.setup_logging()
 
 token = os.environ.get("DISCORD_TOKEN")
 if token is None:
     logging.error("Environment variable 'DISCORD_TOKEN' is not set.")
     sys.exit(1)
 
-client = DiscordClient(intents=intents)
-client.run(token)
+from statesman_discord.bot import bot
+
+bot.run(token)
