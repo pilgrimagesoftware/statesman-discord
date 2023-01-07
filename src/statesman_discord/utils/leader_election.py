416889b2d1df8477c3ed040e676cf7ea7094fb9a
Paul Schifferer <paul@schifferers.net>
2023-01-07 15:34:50 -0800

    Use app object

--- src/statesman_discord/utils/leader_election.py
+++ src/statesman_discord/utils/leader_election.py
@@ -4,7 +4,6 @@ leader.py
 - Leader election utilities
 """
 
-from flask import current_app
 from threading import Thread
 import os
 from statesman_discord import constants
@@ -18,15 +17,18 @@ from kubernetes.leaderelection import electionconfig
 class LeaderElection(object):
     def __init__(self, app) -> None:
         self.is_leader = False
+        self.app = app
 
         if os.environ.get(constants.POD) is None:
             return
 
+        app.logger.info("Loading cluster config...")
         config.load_incluster_config()
         self.candidate_id = os.environ[constants.POD]
         self.lock_name = os.environ.get(constants.LEADER_CONFIGMAP_NAME, f"{os.environ[constants.NAMESPACE]}-leader")
         self.lock_namespace = os.environ[constants.NAMESPACE]
 
+        app.logger.debug("Creating election config...")
         self.election_config = electionconfig.Config(
             ConfigMapLock(self.lock_name, self.lock_namespace, self.candidate_id),
             lease_duration=17,
@@ -39,18 +41,19 @@ class LeaderElection(object):
         # Enter leader election
         self.elect = leaderelection.LeaderElection(self.election_config)
 
+        app.logger.info("Starting election thread...")
         election_thread = Thread(target=self.elect.run)
         election_thread.setDaemon(True)
         election_thread.start()
 
     def _leader_callback(self):
-        current_app.logger.info("I am the leader; executing setup...")
+        self.app.logger.info("I am the leader; executing setup...")
 
         self.is_leader = True
 
         register_commands()
 
     def _follower_callback(self):
-        current_app.logger.info("I am not the leader.")
+        self.app.logger.info("I am not the leader.")
 
         self.is_leader = False
