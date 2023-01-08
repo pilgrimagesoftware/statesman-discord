f0f7e0711cddf7c7fbb5225d1a8216bb6da95cea
Paul Schifferer <paul@schifferers.net>
2023-01-07 21:13:32 -0800

    More generic logging

--- src/statesman_discord/utils/discord/commands.py
+++ src/statesman_discord/utils/discord/commands.py
@@ -5,18 +5,18 @@ commands.py
 """
 
 
-from flask import current_app
+import logging
 import requests
 from statesman_discord.utils.discord import _get_url
 
 
 def register_command(command: object, token: str):
-    current_app.logger.debug("")
+    logging.debug("")
 
     url = _get_url()
-    current_app.logger.debug("url: %s", url)
+    logging.debug("url: %s", url)
 
     headers = {"Authorization": f"Bearer {token}"}
 
     r = requests.post(url, headers=headers, json=command)
-    current_app.logger.info("r: %s", r)
+    logging.info("r: %s", r)
