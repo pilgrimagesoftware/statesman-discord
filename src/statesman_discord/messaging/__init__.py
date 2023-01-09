534eb78afb26dfab9bbee07ff6194fde8b0d594f
Paul Schifferer <paul@schifferers.net>
2023-01-08 18:39:47 -0800

    Threaded message consumer

--- src/statesman_discord/messaging/__init__.py
+++ src/statesman_discord/messaging/__init__.py
@@ -7,6 +7,7 @@ interact
 import pika
 import os, logging, time, json
 from statesman_discord import constants
+from threading import Thread
 
 
 connection = pika.BlockingConnection(
@@ -32,20 +33,31 @@ def send_amqp_message(msg: object):
     body = json.dumps(body_data)
     channel.basic_publish(exchange=os.environ[constants.RABBITMQ_EXCHANGE], routing_key=os.environ[constants.RABBITMQ_API_QUEUE], body=body)
 
-    connection.close()
 
-
-def message_callback(ch, method, properties, body):
+class MessageConsumer(Thread):
     """_summary_
 
     Args:
-        ch (_type_): _description_
-        method (_type_): _description_
-        properties (_type_): _description_
-        body (_type_): _description_
+        Thread (_type_): _description_
     """
-    logging.info("ch: %s, method: %s, properties: %s, body: %s", ch, method, properties, body)
+
+    def message_callback(self, ch, method, properties, body):
+        """_summary_
+
+        Args:
+            ch (_type_): _description_
+            method (_type_): _description_
+            properties (_type_): _description_
+            body (_type_): _description_
+        """
+        logging.info("ch: %s, method: %s, properties: %s, body: %s", ch, method, properties, body)
+
+    def run(self):
+        logging.info("Consumer thread started.")
+        channel.basic_consume(queue=os.environ[constants.RABBITMQ_QUEUE], on_message_callback=self.message_callback, auto_ack=True)
+        channel.start_consuming()
 
 
-channel.basic_consume(queue=os.environ[constants.RABBITMQ_QUEUE], on_message_callback=message_callback, auto_ack=True)
-channel.start_consuming()
+consumer_thread = MessageConsumer()
+consumer_thread.setDaemon(True)
+consumer_thread.start()
