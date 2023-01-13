022d8d97ebb62df59acca5e4755b31ac61945957
Paul Schifferer <paul@schifferers.net>
2023-01-12 20:01:56 -0800

    Handle command parsing properly

--- src/statesman_discord/utils/discord/commands.py
+++ src/statesman_discord/utils/discord/commands.py
@@ -29,8 +29,21 @@ def construct_command(msg: dict) -> str:
     logging.debug("cmd: %s", cmd)
     # TODO: check cmd
 
-    parts = list(map(lambda x: x["name"], msg["data"]["options"]))
+    parts = []
+    for item in msg["data"]["options"]:
+        name = item.get("name")
+        if name:
+            parts.append(name)
+
+            for sub_item in item["options"]:
+                option = sub_item.get("name")
+                value = sub_item.get("value")
+
+                parts.append(f"{option}={value}")
+    # parts = list(map(lambda x: x["name"], msg["data"]["options"]))
     logging.debug("parts: %s", parts)
+    # parts.append(list(map(lambda x: x["name"], msg["data"]["options"][0].get("options", []))))
+    # logging.debug("more parts: %s", parts)
     command = " ".join(parts)
     logging.debug("command: %s", command)
 
